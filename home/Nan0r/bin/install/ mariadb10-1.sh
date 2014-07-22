755f35f1baa7a579daf3f1c485cd999061d15824
Launay <launay.ronan@gmail.com>
2014-07-22 11:06:05 +0200

    Update  mariadb10-1.sh

--- home/Nan0r/bin/install/ mariadb10-1.sh
+++ home/Nan0r/bin/install/ mariadb10-1.sh
@@ -3,3 +3,5 @@ apt-get install python-software-properties
 apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
 add-apt-repository 'deb http://nwps.ws/pub/mariadb/repo/10.1/debian wheezy main'
+apt-get update
+apt-get install mariadb-server
