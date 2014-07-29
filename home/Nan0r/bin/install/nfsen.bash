18f6c8b53e59100645a62af6b215b324e3907a31
Launay <launay.ronan@gmail.com>
2014-07-29 17:28:53 +0200

    nfsen.bash
    nfsen bash install script

---
+++ home/Nan0r/bin/install/nfsen.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+wget http://sourceforge.net/projects/nfsen/files/stable/nfsen-1.3.6p1/nfsen-1.3.6p1.tar.gz
+tar -xvzf nfsen-1.3.6p1.tar.gz
+cd nfsen-1.3.6p1
+cp etc/nfsen-dist.conf etc/nfsen.conf
+useradd --gid www-data --password nfsen nfsen
+mkdir /var/www/nfsen
+chown -R nfsen:www-data /var/www/nfsen
+sed -i -e "s/$BASEDIR = "\/data\/nfsen";/$BASEDIR = "\/opt\/nfsen";/g" etc/nfsen.conf
+sed -i -e "s/$PREFIX = "\/usr\/local\/bin";/$BASEDIR = "\/usr\/bin";/g" etc/nfsen.conf
+sed -i -e "s/$USER    = "netflow";/$USER    = "nfsen";/g" etc/nfsen.conf
+sed -i -e "s/$WWWGROUP = "www";/$WWWGROUP = "www-data";/g" etc/nfsen.conf
+.install.pl etc/nfsen.conf
