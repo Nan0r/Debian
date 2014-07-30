b1644fa7c97e1fc5af84cc525f12d88dc1ca9cd2
Launay <launay.ronan@gmail.com>
2014-07-30 09:52:32 +0200

    Update nfsen.bash

--- home/Nan0r/bin/install/nfsen.bash
+++ home/Nan0r/bin/install/nfsen.bash
@@ -11,5 +11,8 @@ sed -i -e "s/$PREFIX = "\/usr\/local\/bin";/$PREFIX = "\/usr\/bin";/g" etc/nfsen
 sed -i -e "s/$USER    = "netflow";/$USER    = "nfsen";/g" etc/nfsen.conf
 sed -i -e "s/$WWWUSER = "www";/$WWWUSER = "www-data";/g" etc/nfsen.conf
 sed -i -e "s/$WWWGROUP = "www";/$WWWGROUP = "www-data";/g" etc/nfsen.conf
+sed -i -e "s/$MAIL_FROM   = 'your@from.example.net';/$MAIL_FROM = 'nfsen@provider.com';/g" etc/nfsen.conf
+sed -i -e "s/$SMTP_SERVER = 'localhost';/$SMTP_SERVER = 'providersmtprelay';/g" etc/nfsen.conf
 .install.pl etc/nfsen.conf
 ln -s /opt/nfsen/bin/nfsen /etc/init.d/nfsen
+update-rc.d nfsen defaults 20
