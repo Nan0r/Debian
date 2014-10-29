85fea4aab63acd58fab21bc150be33e06da9f2cb
Launay <launay.ronan@gmail.com>
2014-10-29 17:23:53 +0100

    nfsight_install.sh

---
+++ home/Nan0r/bin/install/nfsight_install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+aptitude install libnumber-format-perl picviz libdbd-mysql-perl
+wget http://sourceforge.net/projects/nfsight/files/nfsight-beta-20140905.tgz 
+tar zxvf /home/rlaunay/src/nfsight-beta-20140905.tgz 
+cd nfsight-beta-20140905
+cp /home/rlaunay/src/nfsight-beta-20140905/backend/nfsight.pm /opt/nfsen/plugins/
+mkdir /opt/nfsen/plugins/nfsight
+chgrp -R www-data /var/www/nfsen/plugins/nfsight
+mkdir /var/www/nfsight
+cp -R frontend/* /var/www/nfsight/
+chgrp -R www-data /var/www/nfsight ///// chown -R www-data:www-data /var/www/nfsight/
+chmod g+w /var/www/nfsight/
+chmod g+w /opt/nfsen/plugins/nfsight
+chmod g+w /var/www/nfsen/plugins/nfsight/cache
+chmod g+x /var/www/nfsen/nfsight/bin/biflow2picviz.pl
+mysql -u root -p -e "create database ngsight;"
+mysql -u root -p -e "CREATE USER 'nfsight'@'localalhost';"
+mysql -u root -p -e "GRANT ALL PRIVILEGES ON nfsight.* TO nfsight@'%' IDENTIFIED BY 'P@ssw0rd';"
+mysql -u root -p -e "GRANT ALL PRIVILEGES ON nfsight.* TO nfsight@localhost IDENTIFIED BY 'P@ssw0rd';"
+mysql -u root -p -e "GRANT ALL PRIVILEGES ON nfsight.* TO nfsight@'%' WITH GRANT OPTION;"
+mysql -u root -p -e "FLUSH PRIVILEGES;"
+exit
